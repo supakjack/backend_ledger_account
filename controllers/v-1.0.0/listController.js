f2264bbd56c3994fd1919331d9ae227d66dba0a9
Doraemon Nobi <60160167@go.buu.ac.th>
2020-03-16 20:53:02 +0700

    Update listController.js

--- controllers/v-1.0.0/listController.js
+++ controllers/v-1.0.0/listController.js
@@ -3,7 +3,7 @@ const db = require('../../database/conn');
 const sqlHelper = require('../../helpers/sqlHepler');
 const version = '1.0.0';
 
-exports.addIncomeListByBookId = (req, res) => {
+exports.addIncomeListByBookId = (req, res,updateIncomeListByBookId) => {
   let sql =
     'INSERT INTO la_list ' +
     '(lal_money, lal_type, lal_descript, lal_lab_id) ' +
@@ -12,6 +12,19 @@ exports.addIncomeListByBookId = (req, res) => {
     req.body.money,
     1,
     req.body.descript,
+    req.body.id
+  ]);
+  updateIncomeListByBookId
+};
+
+updateIncomeListByBookId = (req, res) => {
+  let sql =
+    'UPDATE la_book ' +
+    'SET lab_amount = lab_amount+?, lab_income = lab_income+? ' +
+    'WHERE lab_id=? '
+  sqlHelper.builder(201, version, db, res, sql, [
+    req.body.money,
+    req.body.money,
     req.body.id
   ]);
 };
