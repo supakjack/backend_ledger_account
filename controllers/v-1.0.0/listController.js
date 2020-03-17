2bd32a2a7bb1c3442a4cd092ca5008555183f8ab
Doraemon Nobi <60160167@go.buu.ac.th>
2020-03-17 14:07:20 +0700

    Update listController.js
    write transaction by ice

--- controllers/v-1.0.0/listController.js
+++ controllers/v-1.0.0/listController.js
@@ -3,31 +3,67 @@ const db = require('../../database/conn');
 const sqlHelper = require('../../helpers/sqlHepler');
 const version = '1.0.0';
 
-exports.addIncomeListByBookId = (req, res,updateIncomeListByBookId) => {
+exports.addIncomeListByBookId = (req, res,) => {
+// 
+db.beginTransaction(err => {
+  if (err) {
+    throw err;
+  }
   let sql =
     'INSERT INTO la_list ' +
     '(lal_money, lal_type, lal_descript, lal_lab_id) ' +
     'VALUES (?, ? , ? , ? ) ';
-  sqlHelper.builder(201, version, db, res, sql, [
+  db.query(sql, [
     req.body.money,
     1,
     req.body.descript,
-    req.body.id
-  ]);
-  updateIncomeListByBookId
+    req.body.id],
+    (error, query) => {
+      if (error) {
+        return db.rollback(function () {
+          throw error;
+        });
+      }
+      sql =
+        'UPDATE la_book ' +
+        'SET lab_amount = lab_amount+?, lab_income = lab_income+? ' +
+        'WHERE lab_id=? ';
+      db.query(sql, [req.body.money, req.body.money, req.body.id], (error, query) => {
+        if (error) {
+          return db.rollback(function () {
+            throw error;
+          });
+        }
+        db.commit(err => {
+          if (err) {
+            return db.rollback(function () {
+              throw err;
+            });
+          }
+          console.log('success!');
+          res.status(200).json({
+            code: 200,
+            version: version,
+            message: 'Success response',
+            data: query
+          });
+        }); // end commit
+      }); // end delete book
+    }); // end delete list
+}); // end beginTransaction
+// 
 };
-
-updateIncomeListByBookId = (req, res) => {
-  let sql =
-    'UPDATE la_book ' +
-    'SET lab_amount = lab_amount+?, lab_income = lab_income+? ' +
-    'WHERE lab_id=? '
-  sqlHelper.builder(201, version, db, res, sql, [
-    req.body.money,
-    req.body.money,
-    req.body.id
-  ]);
-};
+// updateIncomeListByBookId = (req, res) => {
+//   let sql =
+//     'UPDATE la_book ' +
+//     'SET lab_amount = lab_amount+?, lab_income = lab_income+? ' +
+//     'WHERE lab_id=? '
+//   sqlHelper.builder(201, version, db, res, sql, [
+//     req.body.money,
+//     req.body.money,
+//     req.body.id
+//   ]);
+// };
 
 exports.addExpenseListByBookId = (req, res) => {
   let sql =
